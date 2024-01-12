856644f006c5fabcdb01da7f88ecfbeeec5035bb
Alex Spangher <spangher@D22ML-SPANGHER.local>
2024-01-12 18:05:02 -0500

    e

--- evaluation/evaluate_using_dtw.py
+++ evaluation/evaluate_using_dtw.py
@@ -7,9 +7,15 @@ import pretty_midi
 import scipy
 import IPython
 import os
+import sqlite3
+import pandas as pd
+import random
+import datetime
+import glob
+from tqdm.auto import tqdm
 
 # Audio/CQT parameters
-FS = 22050
+FS = 22050.
 NOTE_START = 36
 N_NOTES = 48
 HOP_LENGTH = 1024
@@ -80,27 +86,109 @@ def load_and_run_dtw(audio_file=None, midi_file=None, audio_packet=None):
     return score, midi_audio_aligned, (audio_cqt, audio_times, audio_data)
 
 
+def match_audio_midi_file_lists(audio_files, midi_files):
+    audio = pd.DataFrame(audio_files, columns=['audio_file'])
+    midi = pd.DataFrame(midi_files, columns=['midi_file'])
+    audio['key'] = audio['audio_file'].apply(os.path.basename).apply(lambda x: x.replace('.wav', ''))
+    midi['key'] = midi['midi_file'].apply(os.path.basename).apply(lambda x: x.replace('_transcribed.mid', ''))
+    matched_files = pd.merge(audio, midi, on='key', how='inner')
+    return matched_files[['audio_file', 'midi_file']].apply(list, axis=1).tolist()
+
+
 if __name__ == "__main__":
-    # We'll use a real-world MIDI/audio pair
-    bach_dir = '/mnt/data10/spangher/aira-dl/data/test-bach-with-diff-models'
-    audio_file = os.path.join(
-        bach_dir,
-        'Johann_Sebastian_Bach_Glenn_Gould_-_The_Well-Tempered_Clavier_Book_1_Fugue_No._6_in_D_Minor_BWV_851.wav'
-    )
-    midi_file = os.path.join(bach_dir, 'bach__baseline-hft.midi')
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-audio_file', help='audio file', default=None)
+    parser.add_argument('-midi_file', help='midi file', default=None)
 
-    score, midi_audio_aligned, audio_proc_packet = load_and_run_dtw(audio_file, midi_file)
-    print("Confidence score: {}".format(score))
+    parser.add_argument('-audio_file_pattern', default=None, help='audio file')
+    parser.add_argument('-midi_file_pattern', default=None, help='audio file')
+    parser.add_argument('-output_sqlite_db', default=None, help='output file')
+    parser.add_argument('-start_index', default=None, help='start index')
+    parser.add_argument('-end_index', default=None, help='end index')
 
-    midi_file = os.path.join(bach_dir, 'bach__aug-hft__003_004.midi')
-    score, midi_audio_aligned, _ = load_and_run_dtw(midi_file=midi_file, audio_packet=audio_proc_packet)
-    print("Confidence score: {}".format(score))
+    args = parser.parse_args()
 
-    midi_file = os.path.join(bach_dir, 'bach__aug-hft__005_006.midi')
-    score, midi_audio_aligned, _ = load_and_run_dtw(audio_file, midi_file)
-    print("Confidence score: {}".format(score))
+    conn = sqlite3.connect(args.output_sqlite_db)
+    c = conn.cursor()
+    c.execute(
+        '''CREATE TABLE IF NOT EXISTS scores 
+        (   audio_file text KEY, 
+            midi_file text KEY,
+            timestamp real,
+            score real
+        )
+    ''')
 
-    # We'll use a real-world MIDI/audio pair
-    midi_file = os.path.join(bach_dir, 'bach__aug-hft__006_009.midi')
-    score, midi_audio_aligned, _ = load_and_run_dtw(audio_file, midi_file)
-    print("Confidence score: {}".format(score))
+    if (args.audio_file is not None) and (args.midi_file is not None):
+        audio_files_to_process = [args.audio_file]
+        midi_files_to_process = [args.midi_file]
+    else:
+        audio_files_to_process = glob.glob(args.audio_file_pattern)
+        midi_files_to_process = glob.glob(args.midi_file_pattern)
+
+    matched_files = match_audio_midi_file_lists(audio_files_to_process, midi_files_to_process)
+    random.shuffle(matched_files)
+    if (args.start_index is not None) and (args.end_index is not None):
+        matched_files = matched_files[args.start_index:args.end_index]
+
+    for audio_file, midi_file in tqdm(matched_files):
+        # check if we've already processed this file
+        c.execute('SELECT * FROM scores WHERE audio_file=? AND midi_file=?', (audio_file, midi_file))
+        if len(c.fetchall()) > 0:
+            continue
+
+        print(audio_file, midi_file)
+        score, _, _ = load_and_run_dtw(audio_file, midi_file)
+        c.execute('INSERT INTO scores VALUES (?, ?, ?, ?)', (audio_file, midi_file, str(datetime.datetime.now()), score))
+        conn.commit()
+
+
+
+
+
+
+
+"""
+python evaluate_using_dtw.py \
+    -audio_file_pattern=/mnt/data10/spangher/aira-dl/corpus/*/*.wav \
+    -midi_file_pattern=/mnt/data10/spangher/aira-dl/data/2024-01-09__full-corpus__006-009-model/*.midi \
+    -output_sqlite_db=/mnt/data10/spangher/aira-dl/data/2024-01-09__full-corpus__006-009-model/scores.db
+
+python /mnt/data10/spangher/aira-dl/hFT-Transformer/evaluation/evaluate_using_dtw.py \
+    -audio_file_pattern=/mnt/data10/spangher/aira-dl/corpus/*/*.wav \
+    -midi_file_pattern=/mnt/data10/spangher/aira-dl/data/2024-01-09__full-corpus__006-009-model/*.mid \
+    -output_sqlite_db=/nas/home/spangher/scores.db
+    
+python /mnt/data10/spangher/aira-dl/hFT-Transformer/evaluation/evaluate_using_dtw.py \
+    -audio_file_pattern=/mnt/data10/spangher/aira-dl/hFT-Transformer/evaluation/glenn-gould-bach-data/*.wav \
+    -midi_file_pattern=/mnt/data10/spangher/aira-dl/hFT-Transformer/evaluation/glenn-gould-bach-data/*/*.mid \
+    -output_sqlite_db=/nas/home/spangher/bach-eval-scores.db
+"""
+
+"""
+one doc sample:
+
+# We'll use a real-world MIDI/audio pair
+bach_dir = '/mnt/data10/spangher/aira-dl/data/test-bach-with-diff-models'
+audio_file = os.path.join(
+    bach_dir,
+    'Johann_Sebastian_Bach_Glenn_Gould_-_The_Well-Tempered_Clavier_Book_1_Fugue_No._6_in_D_Minor_BWV_851.wav'
+)
+midi_file = os.path.join(bach_dir, 'bach__baseline-hft.midi')
+
+score, midi_audio_aligned, audio_proc_packet = load_and_run_dtw(audio_file, midi_file)
+print("Confidence score: {}".format(score))
+
+midi_file = os.path.join(bach_dir, 'bach__aug-hft__003_004.midi')
+score, midi_audio_aligned, _ = load_and_run_dtw(midi_file=midi_file, audio_packet=audio_proc_packet)
+print("Confidence score: {}".format(score))
+
+midi_file = os.path.join(bach_dir, 'bach__aug-hft__005_006.midi')
+score, midi_audio_aligned, _ = load_and_run_dtw(audio_file, midi_file)
+print("Confidence score: {}".format(score))
+
+# We'll use a real-world MIDI/audio pair
+midi_file = os.path.join(bach_dir, 'bach__aug-hft__006_009.midi')
+score, midi_audio_aligned, _ = load_and_run_dtw(audio_file, midi_file)
+print("Confidence score: {}".format(score))"""
--- evaluation/transcribe_new_files.py
+++ evaluation/transcribe_new_files.py
@@ -9,6 +9,7 @@ sys.path.append(os.getcwd())
 from model import amt
 from pydub import AudioSegment
 from pydub.exceptions import CouldntDecodeError
+import random
 
 try:
     from speechbrain.pretrained import SpectralMaskEnhancement
@@ -38,6 +39,9 @@ if __name__ == '__main__':
     parser.add_argument('-output_file', default=None, help='output file')
     parser.add_argument('-f_config', help='config json file', default='../corpus/config.json')
     parser.add_argument('-model_file', help='input model file', default='best_model.pkl')
+    parser.add_argument('-start_index', help='start index', type=int, default=None)
+    parser.add_argument('-end_index', help='end index', type=int, default=None)
+    parser.add_argument('-skip_transcribe_mp3', action='store_true', default=False)
     # parameters
     parser.add_argument('-mode', help='mode to transcript (combination|single)', default='combination')
     parser.add_argument('-thred_mpe', help='threshold value for mpe detection', type=float, default=0.5)
@@ -50,14 +54,27 @@ if __name__ == '__main__':
     assert (args.input_dir_to_transcribe is not None) or (args.input_file_to_transcribe is not None), "input file or directory is not specified"
 
     if args.input_dir_to_transcribe is not None:
-        # list file
-        a_mp3s = (
-            glob.glob(os.path.join(args.input_dir_to_transcribe, '*.mp3')) +
-            glob.glob(os.path.join(args.input_dir_to_transcribe, '*', '*.mp3'))
+        if not args.skip_transcribe_mp3:
+            # list file
+            a_mp3s = (
+                    glob.glob(os.path.join(args.input_dir_to_transcribe, '*.mp3')) +
+                    glob.glob(os.path.join(args.input_dir_to_transcribe, '*', '*.mp3'))
+            )
+            print(f'transcribing {len(a_mp3s)} files: [{str(a_mp3s)}]...')
+            list(map(check_and_convert_mp3_to_wav, a_mp3s))
+
+        a_list = (
+            glob.glob(os.path.join(args.input_dir_to_transcribe, '*.wav')) +
+            glob.glob(os.path.join(args.input_dir_to_transcribe, '*', '*.wav'))
         )
-        print(f'transcribing {len(a_mp3s)} files: [{str(a_mp3s)}]...')
-        list(map(check_and_convert_mp3_to_wav, a_mp3s))
-        a_list = glob.glob(os.path.join(args.input_dir_to_transcribe, '*.wav'))
+        if (args.start_index is not None) or (args.end_index is not None):
+            if args.start_index is None:
+                args.start_index = 0
+            if args.end_index is None:
+                args.end_index = len(a_list)
+            a_list = a_list[args.start_index:args.end_index]
+        # shuffle a_list
+        random.shuffle(a_list)
 
     elif args.input_file_to_transcribe is not None:
         args.input_file_to_transcribe = check_and_convert_mp3_to_wav(args.input_file_to_transcribe)
@@ -75,7 +92,6 @@ if __name__ == '__main__':
 
     long_filename_counter = 0
     for fname in a_list:
-        print('[' + fname + ']')
         if args.output_file is not None:
             output_fname = args.output_file
         else:
@@ -87,42 +103,47 @@ if __name__ == '__main__':
             if os.path.exists(output_fname):
                 continue
 
-        a_feature = AMT.wav2feature(fname)
+        print('[' + fname + ']')
+        try:
+            a_feature = AMT.wav2feature(fname)
 
-        # transcript
-        if args.n_stride > 0:
-            output = AMT.transcript_stride(a_feature, args.n_stride, mode=args.mode, ablation_flag=args.ablation)
-        else:
-            output = AMT.transcript(a_feature, mode=args.mode, ablation_flag=args.ablation)
-        (output_1st_onset, output_1st_offset, output_1st_mpe, output_1st_velocity,
-         output_2nd_onset, output_2nd_offset, output_2nd_mpe, output_2nd_velocity) = output
+            # transcript
+            if args.n_stride > 0:
+                output = AMT.transcript_stride(a_feature, args.n_stride, mode=args.mode, ablation_flag=args.ablation)
+            else:
+                output = AMT.transcript(a_feature, mode=args.mode, ablation_flag=args.ablation)
+            (output_1st_onset, output_1st_offset, output_1st_mpe, output_1st_velocity,
+             output_2nd_onset, output_2nd_offset, output_2nd_mpe, output_2nd_velocity) = output
 
-        # note (mpe2note)
-        a_note_1st_predict = AMT.mpe2note(
-            a_onset=output_1st_onset,
-            a_offset=output_1st_offset,
-            a_mpe=output_1st_mpe,
-            a_velocity=output_1st_velocity,
-            thred_onset=args.thred_onset,
-            thred_offset=args.thred_offset,
-            thred_mpe=args.thred_mpe,
-            mode_velocity='ignore_zero',
-            mode_offset='shorter'
-        )
+            # note (mpe2note)
+            a_note_1st_predict = AMT.mpe2note(
+                a_onset=output_1st_onset,
+                a_offset=output_1st_offset,
+                a_mpe=output_1st_mpe,
+                a_velocity=output_1st_velocity,
+                thred_onset=args.thred_onset,
+                thred_offset=args.thred_offset,
+                thred_mpe=args.thred_mpe,
+                mode_velocity='ignore_zero',
+                mode_offset='shorter'
+            )
 
-        a_note_2nd_predict = AMT.mpe2note(
-            a_onset=output_2nd_onset,
-            a_offset=output_2nd_offset,
-            a_mpe=output_2nd_mpe,
-            a_velocity=output_2nd_velocity,
-            thred_onset=args.thred_onset,
-            thred_offset=args.thred_offset,
-            thred_mpe=args.thred_mpe,
-            mode_velocity='ignore_zero',
-            mode_offset='shorter'
-        )
+            a_note_2nd_predict = AMT.mpe2note(
+                a_onset=output_2nd_onset,
+                a_offset=output_2nd_offset,
+                a_mpe=output_2nd_mpe,
+                a_velocity=output_2nd_velocity,
+                thred_onset=args.thred_onset,
+                thred_offset=args.thred_offset,
+                thred_mpe=args.thred_mpe,
+                mode_velocity='ignore_zero',
+                mode_offset='shorter'
+            )
 
-        AMT.note2midi(a_note_2nd_predict, output_fname)
+            AMT.note2midi(a_note_2nd_predict, output_fname)
+        except Exception as e:
+            print(e)
+            continue
 
     print('** done **')
 
@@ -131,10 +152,22 @@ if __name__ == '__main__':
 python evaluation/transcribe_new_files.py \
     -input_dir_to_transcribe ../data/test-album \
     -output_dir ../data/test-album-transcribed \
-    -f_config corpus/MAESTRO-V3/dataset/config-aug.json \
+    -f_config corpus/MAESTRO-V3/dataset/config-orig.json \
     -model_file evaluation/checkpoint/MAESTRO-V3/model_016_003.pkl
     
     
+## transcribe full corpus
+
+python evaluation/transcribe_new_files.py \
+    -input_dir_to_transcribe ../data/corpus \
+    -output_dir ../data/2024-01-09__full-corpus__006-009-model \
+    -f_config corpus/MAESTRO-V3/dataset/config-aug.json \
+    -model_file evaluation/checkpoint/MAESTRO-V3/model-with-aug-data_006_009.pkl    
+    
+    
+    
+## evaluate test sample
+
 python evaluation/transcribe_new_files.py \
     -input_file_to_transcribe ../data/Johann_Sebastian_Bach_Glenn_Gould_-_The_Well-Tempered_Clavier_Book_1_Fugue_No._6_in_D_Minor_BWV_851.mp3 \
     -output_file ../data/test-bach-with-diff-models/bach__baseline-hft.midi \
@@ -158,4 +191,35 @@ python evaluation/transcribe_new_files.py \
     -output_file ../data/test-bach-with-diff-models/bach__aug-hft__006_009.midi \
     -f_config corpus/MAESTRO-V3/dataset/config-aug.json \
     -model_file evaluation/checkpoint/MAESTRO-V3/model-with-aug-data_006_009.pkl
+
+python evaluation/transcribe_new_files.py \
+    -input_dir_to_transcribe evaluation/glenn-gould-bach-data \
+    -output_dir evaluation/glenn-gould-bach-data/base-model \
+    -f_config corpus/MAESTRO-V3/dataset/config-orig.json \
+    -model_file evaluation/checkpoint/MAESTRO-V3/model_016_003.pkl
+    
+python evaluation/transcribe_new_files.py \
+    -input_dir_to_transcribe evaluation/glenn-gould-bach-data \
+    -output_dir evaluation/glenn-gould-bach-data/aug-model-006-009 \
+    -f_config corpus/MAESTRO-V3/dataset/config-aug.json \
+    -model_file evaluation/checkpoint/MAESTRO-V3/model-with-aug-data_006_009.pkl    
+
+box 11: 1727 - 2173 running final sweep
+box 12: 2173 - 2619 running final sweep
+box 13: 2619 - 3065 still running
+box 14: 3065 - 3511  running final sweep
+box 10 : 3511 - 3957 running final sweep
+box 9 : 3957 - 4403 still running
+box 7: 4403 - 4849 running final sweep
+box 6: 4849 - 5295 running final sweep
+box 5: 5295 - 5741 running final sweep
+box 4: 5741 - 6185 still running
+
+cd /mnt/data10/spangher/aira-dl/hFT-Transformer
+python evaluation/transcribe_new_files.py \
+    -input_dir_to_transcribe '../corpus/*' \
+    -output_dir ../data/2024-01-09__full-corpus__006-009-model/ \
+    -f_config corpus/MAESTRO-V3/dataset/config-aug.json \
+    -model_file evaluation/checkpoint/MAESTRO-V3/model-with-aug-data_006_009.pkl \
+    -start_index 5741 -end_index 6185
 """
